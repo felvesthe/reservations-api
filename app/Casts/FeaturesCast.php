36d00c56fae39be91d128766ea503778477b6853
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-02-12 22:09:06 +0100

    feat(features-cast): support json and array input of features
    - added support for JSON string parsing in set method
    - added support for array input transformation in set method
    - added check for null value in get method
    - disabled object caching to prevent issues
    
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- app/Casts/FeaturesCast.php
+++ app/Casts/FeaturesCast.php
@@ -18,15 +18,23 @@ use JsonException;
  */
 final class FeaturesCast implements CastsAttributes
 {
+    public bool $withoutObjectCaching = true;
+
     /**
      * @param  Reservable  $model
-     * @param  string  $value
+     * @param  string  $key
+     * @param  string|null  $value
      * @param  array<string, mixed>  $attributes
+     * @return FeatureData|null
      * @throws JsonException
      */
-    public function get(Model $model, string $key, mixed $value, array $attributes): FeatureData
+    public function get(Model $model, string $key, mixed $value, array $attributes): ?FeatureData
     {
-        /** @var array<string, bool|int|string> $data */
+        if (null === $value) {
+            return null;
+        }
+
+        /** @var array<string, mixed> $data */
         $data = json_decode(
             json: $value,
             associative: true,
@@ -47,13 +55,40 @@ final class FeaturesCast implements CastsAttributes
      * @param  mixed  $value
      * @param  array<string, mixed>  $attributes
      * @return string
+     * @throws JsonException
      */
     public function set(Model $model, string $key, mixed $value, array $attributes): string
     {
-        if (! $value instanceof FeatureData) {
-            throw new InvalidArgumentException(__('exceptions.reservable.features.not_instance_of'));
+        /** @var FeatureStrategyContext $strategyService */
+        $strategyService = FeatureStrategyFactory::create(
+            reservableType: $model->type->value,
+            reservable: $model,
+        );
+
+        if (is_string($value) && json_validate($value)) {
+            /** @var array<string, mixed> $data */
+            $data = json_decode(
+                json: $value,
+                associative: true,
+                flags: JSON_THROW_ON_ERROR,
+            );
+
+            return $strategyService
+                ->make($data)
+                ->toJson();
         }
 
-        return $value->toJson();
+        if (is_array($value)) {
+            /** @var array<string, mixed> $value */
+            return $strategyService
+                ->make($value)
+                ->toJson();
+        }
+
+        if ($value instanceof FeatureData) {
+            return $value->toJson();
+        }
+
+        throw new InvalidArgumentException(__('exceptions.reservable.features.not_supported'));
     }
 }
