07b9d0e58fb96f790c270532f1fe40dfefbb19e5
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-08 13:41:11 +0100

    fix(rate-limiter): cast config value to int
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- config/rate-limiter.php
+++ config/rate-limiter.php
@@ -6,5 +6,5 @@ return [
     /**
      * Rate limit per minute for API
      */
-    'api_rate_limit_per_minute' => env('API_RATE_LIMIT_PER_MINUTE', 60),
+    'api_rate_limit_per_minute' => (int) env('API_RATE_LIMIT_PER_MINUTE', 60),
 ];
