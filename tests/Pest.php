6629c29d58e7c2bbde3c86165c215fe27880e2f6
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-12 23:41:02 +0100

    chore(pest): let unit tests extend TestCase class
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -14,7 +14,9 @@ declare(strict_types=1);
 */
 
 pest()->extend(Tests\TestCase::class)
-    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Feature', 'Unit');
+
+pest()->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
 /*
